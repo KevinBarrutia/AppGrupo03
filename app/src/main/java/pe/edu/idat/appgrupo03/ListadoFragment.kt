5d3bb2229841d74070763b43411354e90e28b3aa
Roxana <a19103729@idat.edu.pe>
2024-03-17 00:12:12 -0500

    Implementación y configuración inicial del RecyclerView

--- app/src/main/java/pe/edu/idat/appgrupo03/ListadoFragment.kt
+++ app/src/main/java/pe/edu/idat/appgrupo03/ListadoFragment.kt
@@ -1,8 +1,43 @@
 package pe.edu.idat.appgrupo03
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ListadoFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: ResenaAdapter
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_listado, container, false)
+        recyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        adapter = ResenaAdapter(generateSampleData())
+        recyclerView.adapter = adapter
+        return view
+    }
 
+    private fun generateSampleData(): List<Resena> {
+        val data = mutableListOf<Resena>()
+
+        for (i in 1..10) {
+            data.add(
+                Resena(
+                    nombre = "Usuario $i",
+                    resena = "Reseña de ejemplo $i",
+                    fecha = "Fecha $i",
+                    estrellas = (i % 5) + 1
+                )
+            )
+        }
+
+        return data
+    }
 }
